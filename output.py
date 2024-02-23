25c27b86e1d48c446a8e84aeb01242712376ef93
Nathan Pietrantonio <pietrann@uci.edu>
2024-02-23 10:31:53 -0800

    Remove comments

--- output.py
+++ output.py
@@ -1,14 +1,4 @@
 import pyttsx3
-#`145`
-# engine = pyttsx3.init()
-# rate = engine.getProperty('rate')
-# voices = engine.getProperty('voices')
-# engine.setProperty('rate', 200)
-# engine.setProperty('voice', voices[9].id)
-# engine.say("Are u the strongest because you are gojo satoru, or are you gojo satoru because you are the strongest")
-# engine.runAndWait()
-
-
 
 def read_text(response_text):
     engine = pyttsx3.init()
